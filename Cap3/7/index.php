54d4e2cca845ff04d3fdd972aa0c9e8a7d87d610
Jonison Rebelatto <jonisonrebelatto@hotmail.com>
2015-03-12 13:12:57 -0300

    Criando o diretorio 7 e criando o html do Site a Jato

---
+++ Cap3/7/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8"/>
+    <title>Fenix TI - Site a Jato! </title>
+</head>
+
+<body>
+    <div id="estrutura">
+        <div id="topo">
+            <h1>Fenix TI - Site a Jato</h1>
+            <div id="menu">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Empresa</a></li>
+                    <li><a href="#">Produtos</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </div>
+        </div>
+
+
+
+    </div>
+
+</body>
+
+
+</html>
