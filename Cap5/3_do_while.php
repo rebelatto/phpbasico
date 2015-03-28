8b3375d717b96d00f6f3d1b840e058b0d2ad3a44
Jonison Rebelatto <jonisonrebelatto@hotmail.com>
2015-03-28 16:08:45 -0300

    Laço de Repetiçao - DO WHILE - renomeando arquivo index.php para 3_do_while.php

--- Cap5/3_do_while.php
+++ Cap5/3_do_while.php
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php
+//Do While executa pelo menos uma vez
 $x = 0;
 do{
     echo "Faça alguma coisa <br>";
     $x ++;
 }while($x < 5);
 ?>
-=======
->>>>>>> origin/developer
-=======
->>>>>>> master
