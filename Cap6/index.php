0ae33c7fe0224282cde844bc7e8fb3dd51b06643
Jonison Rebelatto <jonisonrebelatto@hotmail.com>
2015-03-30 17:16:21 -0300

    Renomeação errônea de arquivo - removendo o arquivo 1_array.php

--- Cap6/index.php
+++ Cap6/index.php
@@ -1,7 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: rebelatto
- * Date: 28/03/15
- * Time: 18:55
- */
+#####################
+##  Arrays Simples ##
+#####################
+
+$x[0][0] = "Oi";
+$x[0][1] = "Posição 1";
+
+
+//echo $x[0][0];
+
+$animais = array("Gato","Cachorro","Coelho");// índices são implícitos - automáticos
+
+//echo $animais[$i];
+
+$nomes = array(0=>"Pedro",1=>"Wesley",2=>"Mariana",3=>"Raffy");// índices são explicitos - manuais
+
+//echo $nomes[3];
+
+$nomes2 = array(
+    0=>array(
+        0=>"Pedrão",
+        1=>"Rebelatto"),
+    1=>"Posição");
+
+//echo $nomes2[0][1];
+
+###########################
+##  Arrays associativos  ##
+###########################
+
+$config = array();//declaração de arrray
+echo $config;
+?>
