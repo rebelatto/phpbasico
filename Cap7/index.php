53b5157544243e796e2ad8dcc8091b6983948dd7
Jonison Rebelatto <jonisonrebelatto@hotmail.com>
2015-04-07 18:38:59 -0300

    Funções - Funções básicas

--- Cap7/index.php
+++ Cap7/index.php
@@ -1,3 +1,31 @@
+<!DOCTYPE HTML>
+<meta charset="utf-8"/>
 <?php
 //Funções
 
+/**
+ * Primeira versão da função
+ * */
+
+function somaV1(){
+    echo 5 + 5;
+}
+
+
+/**
+ * Função um pouco melhorada
+ * */
+
+function somaV2(){
+$x = 5;
+$y = 6;
+echo $x + $y."<br>";
+}
+
+
+echo "<h3>Função soma V1</h3>";
+somaV1();
+
+echo "<h3>Função soma V2</h3>";
+somaV2();
+?>
