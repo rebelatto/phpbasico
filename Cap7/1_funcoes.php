5efe8add19acf92c5275068854ab24fc56e644eb
Jonison Rebelatto <jonisonrebelatto@hotmail.com>
2015-04-13 17:22:15 -0300

    Funções - Fim do estudo de funções e renomeando arquivo index.php para 1_funcoes.php

--- Cap7/1_funcoes.php
+++ Cap7/1_funcoes.php
@@ -1,5 +1,6 @@
 <!DOCTYPE HTML>
 <meta charset="utf-8"/>
+
 <?php
 //Funções
 
@@ -30,27 +31,16 @@ echo "<h4 style= color:dimgray>Função soma V2</h4>";
 somaV2();
 
 /**
-<<<<<<< HEAD
-<<<<<<< HEAD
  * Variáveis Globais e Variáveis de Escopo
  *
  */
-$x = 10;
-function somaV3($x,$y){
-    echo $x + $y;
-}
-=======
-=======
->>>>>>> developer
- * Terceira versão da função soma - Função utilizando variáveis Globais e variáveis de Escopo
- **/
-
 echo "<h3>Funções avançadas parte I - Variávies Globais e de Escopo</h3>";
 
 
 $x = 10;//Global - Para o documento inteiro
 
 echo "<h4 style= color:dimgray>Função soma V3</h4>";
+
 function somaV3($x = 0,$y = 0){//váriáveis de escopo
     echo $x + $y;
 }
@@ -58,11 +48,12 @@ somaV3(10);
 //Formatos que podemos passar em nossos parâmetros de uma função : int, "String", false/true, null, array( )
 
 /**
- * Função média - Função utilizando variáveis Globais e variáveis de Escopo
+ * Função média - Funções utilizando retorno
  **/
+
 echo "<h3>Funções avançadas parte I - Funções com retorno</h3>";
 
-echo "<h4 style= color:dimgray>Função média</h4>";
+echo "<h4 style= color:dimgray>Função média - Simples</h4>";
 
 function media($p1,$p2,$p3,$p4){
     $resultado = ($p1 + $p2 + $p3 + $p4) / 4;
@@ -72,8 +63,29 @@ function media($p1,$p2,$p3,$p4){
 $media = media(8,5,7,10);
 
 echo "<strong>Sua média é: ".$media."</strong>";
-<<<<<<< HEAD
->>>>>>> developer
-=======
->>>>>>> developer
+
+echo "<h4 style= color:dimgray>Função média - Utilizando Array</h4>";
+
+$alunos[0]["nome"] = "Pedrinho";
+$alunos[0]["media"] = media(5,6,8,9);
+
+$alunos[1]["nome"] = "Márcia";
+$alunos[1]["media"] = media(8,7,8,9);
+
+$alunos[2]["nome"] = "Antônio";
+$alunos[2]["media"] = media(9,7,10,9);
+
+$alunos[3]["nome"] = "Jéssica";
+$alunos[3]["media"] = media(3,8,7,6);
+
+$tam = count($alunos);
+
+for ($i = 0; $i < $tam; $i++){
+    echo "<strong>Nome do Aluno: </strong><em>".$alunos[$i]["nome"]."</em><br>";
+    if ($alunos[$i]["media"] >= 7)
+        echo "<strong>Média Final: </strong><em><spam style = color:#3b00ff>" .$alunos[$i]["media"]."</spam></em><br><br>";
+    else
+        echo "<strong>Média Final: </strong><em><spam style = color:#ff0024>" .$alunos[$i]["media"]."</spam></em><br><br>";
+}
+
 ?>
